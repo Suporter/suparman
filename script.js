ca948ef6cf56055f74e4a20cb71565918029a23a
pollonet <30765345+pollonet@users.noreply.github.com>
2019-04-25 16:18:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+<script type="text/javascript">
+    var go_url = 'https://suarankri.me/';
+    var api = '8dfe678b2c7b300c005e01bc2b5c98a008dd13bc';
+    var shorten_exclude = ['google.com', 'mysite.com', 'domain.com']; 
+</script>
+<script function go_get_url(url){var l=document.createElement("a");l.href=url;return l};function go_get_host_name(url){var domain;if(typeof url==='undefined'||url===null||url===''||url.match(/^\#/)){return ""}
+url=go_get_url(url);if(url.href.search(/^http[s]?:\/\//)!==-1){domain=url.href.split('/')[2]}else{return ""}
+domain=domain.split(':')[0];return domain.toLowerCase()}
+document.addEventListener("DOMContentLoaded",function(event){if(typeof go_url==='undefined'){return}
+if(typeof api==='undefined'){return}
+var advert_type=2;var anchors=document.getElementsByTagName("a");if(typeof shorten_includ!=='undefined'){for(var i=0;i<anchors.length;i++){var hostname=go_get_host_name(anchors[i].getAttribute("href"));if(hostname.length>0&&shorten_includ.indexOf(hostname)>-1){anchors[i].href=go_url+"full/?api="+encodeURIComponent(api)+"&url="+encodeURIComponent(anchors[i].href)+"&type="+encodeURIComponent(advert_type)}else{if(anchors[i].protocol==="magnet:"){anchors[i].href=go_url+"full/?api="+encodeURIComponent(api)+"&url="+encodeURIComponent(anchors[i].href)+"&type="+encodeURIComponent(advert_type)}}}
+return}
+if(typeof shorten_exclude!=='undefined'){for(var i=0;i<anchors.length;i++){var hostname=go_get_host_name(anchors[i].getAttribute("href"));if(hostname.length>0&&shorten_exclude.indexOf(hostname)===-1){anchors[i].href=go_url+"full/?api="+encodeURIComponent(api)+"&url="+encodeURIComponent(anchors[i].href)+"&type="+encodeURIComponent(advert_type)}else{if(anchors[i].protocol==="magnet:"){anchors[i].href=go_url+"full/?api="+encodeURIComponent(api)+"&url="+encodeURIComponent(anchors[i].href)+"&type="+encodeURIComponent(advert_type)}}}
+return}})></script>
